9c9557adfa058049e47ea553d5b2d882759f8627
Anton Kokorin <ant.kkrn@gmail.com>
2025-03-25 21:28:25 +0300

    test ci/cd

--- backend/routes.py
+++ backend/routes.py
@@ -4,11 +4,6 @@ from fastapi.responses import FileResponse, RedirectResponse
 router = APIRouter()
 
 
-# @router.get("/")
-# async def serve_react_app():
-#     return FileResponse("../frontend/build/index.html")
-
-
 @router.get("/admin/")
 async def redirect_to_admin_dashboard():
     return RedirectResponse(url="/admin/card/list")
