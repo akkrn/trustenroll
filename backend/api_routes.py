a0784cf0e537deb18861929fb2856443f3b6e9d5
Anton Kokorin <ant.kkrn@gmail.com>
2025-03-28 22:34:11 +0300

    add sorting key len(cards) in bank

--- backend/api_routes.py
+++ backend/api_routes.py
@@ -23,15 +23,22 @@ def sorting_and_ordering_cards(cards):
     grouped = defaultdict(list)
     for card in cards:
         grouped[card.bank_name].append(card)
+
+    def bank_sort_key(item):
+        bank_name, card_list = item
+        total_balance = sum(extract_available(c.card_name) for c in card_list)
+        return (-len(card_list), -total_balance)  # минус для сортировки по убыванию
+
+    sorted_grouped = sorted(grouped.items(), key=bank_sort_key)
+
     response = [
         {
             "bank_name": bank,
             "cards": sorted([CardSchema.from_orm(c) for c in card_list], key=lambda c: extract_available(c.card_name), reverse=True),
         }
-        for bank, card_list in grouped.items()
+        for bank, card_list in sorted_grouped
     ]
-    response = jsonable_encoder(response)
-    return response
+    return jsonable_encoder(response)
 
 
 @api_router.get("/all_cards", response_model=List[BankCardsSchema])
@@ -84,6 +91,7 @@ async def get_main_category_details(main_category_id: int):
 
 @api_router.post("/ping")
 async def track_visit(request: Request):
+    print(request.headers)
     x_forwarded_for = request.headers.get("X-Forwarded-For")
     ip = x_forwarded_for.split(",")[0] if x_forwarded_for else request.client.host
     ua_string = request.headers.get("User-Agent", "")
